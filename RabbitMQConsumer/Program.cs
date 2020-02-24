a7a92f06f7a64e5247623e405259e1591a513c6d
Your Name <you@example.com>
2020-02-24 17:10:56 -0300

    Adicionar arquivos de projeto.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "40.78.128.18", UserName = "Teste", Password = "teste" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    channel.QueueDeclare(
+                        queue: "SampleJA",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(message);
+                    };
+                    channel.BasicConsume(queue: "SampleJA",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine("ConsumerOK");
+                    Console.ReadLine();
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
